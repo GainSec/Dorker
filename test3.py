a1c4a55c3f32d40be96187e10f613b933e00ed14
GainSec <46727209+gaingithubsec@users.noreply.github.com>
2020-05-20 20:34:59 -0400

    Update test3.py
    Prototype 3

--- test3.py
+++ test3.py
@@ -3,30 +3,31 @@ import re
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--s', help='search for domain "in site"')
+# parser.add_argument('--s', help='search for domain "in site"')
 parser.add_argument('--f', help='search for a specific file type. note: use , for AND...use . for OR')
-parser.add_argument('--e', help='search for file extension.       note: use , for AND...use . for OR')
+# parser.add_argument('--e', help='search for file extension.       note: use , for AND...use . for OR')
 parser.add_argument('--k', help='keyword                          note: use , for AND...use . for OR')
-parser.add_argument('--a', help='all in url')
-parser.add_argument('--i', help='in url')
-parser.add_argument('--A', help='AND keyword')
+# parser.add_argument('--a', help='all in url')
+# parser.add_argument('--i', help='in url')
+# parser.add_argument('--A', help='AND keyword')
 parser.add_argument('--d', help='domain searches')
 
 args = parser.parse_args()
 
 # print(args)
 # print(type(args.k))
-s             = args.s
+# s             = args.s
 f             = args.f
 k             = args.k
-a             = args.a
-i             = args.i
-e             = args.e
-d             = args.d
+# a             = args.a
+# i             = args.i
+# e             = args.e
+s             = args.d
 GLOBAL_STRING = ""
 arg_count     = 0
 dork_types    = 0
 output_string = ""
+misc          = False
 # func that takes site as param and outputs a single dork
 
 def dorks():
@@ -61,12 +62,26 @@ def dorks():
     c4D = ""
     c4B = ""
     c4X = ""
-    c4U = ""     
+    c4U = ""   
+
+    c5G = ""
+    c5Y = ""
+    c5D = ""
+    c5B = ""
+    c5X = ""
+    c5U = "" 
+
+    c99G = ""
+    c99Y = ""
+    c99D = ""
+    c99B = ""
+    c99X = ""
+    c99U = ""         
     # ---------------------------
     if s is not None:
-        cat1_out += " site:{}".format(s)
-        cat2_out += " allinurl:{}".format(s)
-        cat3_out += " inurl:{}".format(s)
+        # cat1_out += " site:{}".format(s)
+        # cat2_out += " allinurl:{}".format(s)
+        # cat3_out += " inurl:{}".format(s)
         c1G += " site:{}".format(s)
         c1Y += " site:{}".format(s)
         c1D += " site:{}".format(s)
@@ -88,12 +103,12 @@ def dorks():
         c3X += ""
         c3U += " inurl:{}".format(s)
 
-        c4G += " cache:{}".format(s)
-        c4Y += ""
-        c4D += " domain {}".format(s)
-        c4B += ""
-        c4X += ""
-        c4U += " inanchor:{}".format(s) 
+        c99G += " cache:{}".format(s)
+        c99Y += ""
+        c99D += " domain {}".format(s)
+        c99B += ""
+        c99X += ""
+        c99U += " inanchor:{}".format(s) 
     if f is not None:
         if ',' in f:
             x = f.split(',')
@@ -211,9 +226,37 @@ def dorks():
         if ',' in k:
             x = k.split(',')
             for i in range(len(x)):
-                cat1_out += ' "{}"'.format(x[i])
-                cat2_out += ' "{}"'.format(x[i])
-                cat3_out += ' "{}"'.format(x[i])
+                c1G += ' "{}"'.format(x[i])
+                c1Y += ' "{}"'.format(x[i])
+                c1D += ' "{}"'.format(x[i])
+                c1B += ' "{}"'.format(x[i])
+                c1X += ' "{}"'.format(x[i])
+                c1U += ' "{}"'.format(x[i])
+                c2G += ' "{}"'.format(x[i])
+                c2Y += ' "{}"'.format(x[i])
+                c2D += ' "{}"'.format(x[i])
+                c2B += ' "{}"'.format(x[i])
+                c2X += ' "{}"'.format(x[i])
+                c2U += ' "{}"'.format(x[i])
+                c3G += ' "{}"'.format(x[i])
+                c3Y += ' "{}"'.format(x[i])
+                c3D += ' "{}"'.format(x[i])
+                c3B += ' "{}"'.format(x[i])
+                c3X += ' "{}"'.format(x[i])
+                c3U += ' "{}"'.format(x[i])
+                c4G += ' "{}"'.format(x[i])
+                c4Y += ' "{}"'.format(x[i])
+                c4D += ' "{}"'.format(x[i])
+                c4B += ' "{}"'.format(x[i])
+                c4X += ' "{}"'.format(x[i])
+                c4U += ' "{}"'.format(x[i])
+                c5G += ' "{}"'.format(x[i])
+                c5Y += ' "{}"'.format(x[i])
+                c5D += ' "{}"'.format(x[i])
+                c5B += ' "{}"'.format(x[i])
+                c5X += ' "{}"'.format(x[i])
+                c5U += ' "{}"'.format(x[i])
+
                 if i < len(x) - 1:
                     c1G += " AND"
                     c1Y += " AND"
@@ -235,32 +278,54 @@ def dorks():
                     c3B += " AND"
                     c3X += " AND"
                     c3U += " AND"
+
+                    c4G += " AND"
+                    c4Y += " AND"
+                    c4D += " AND"
+                    c4B += " AND"
+                    c4X += " AND"
+                    c4U += " AND"
+
+                    c5G += " AND"
+                    c5Y += " AND"
+                    c5D += " AND"
+                    c5B += " AND"
+                    c5X += " AND"
+                    c5U += " AND"
+
         elif '.' in k:
             x = k.split('.')
             for i in range(len(x)):
-                cat1_out += ' "{}"'.format(x[i])
-                cat2_out += ' "{}"'.format(x[i])
-                cat3_out += ' "{}"'.format(x[i])
                 c1G += ' "{}"'.format(x[i])
                 c1Y += ' "{}"'.format(x[i])
                 c1D += ' "{}"'.format(x[i])
                 c1B += ' "{}"'.format(x[i])
                 c1X += ' "{}"'.format(x[i])
                 c1U += ' "{}"'.format(x[i])
-                
                 c2G += ' "{}"'.format(x[i])
                 c2Y += ' "{}"'.format(x[i])
                 c2D += ' "{}"'.format(x[i])
                 c2B += ' "{}"'.format(x[i])
                 c2X += ' "{}"'.format(x[i])
                 c2U += ' "{}"'.format(x[i])
-
                 c3G += ' "{}"'.format(x[i])
                 c3Y += ' "{}"'.format(x[i])
                 c3D += ' "{}"'.format(x[i])
                 c3B += ' "{}"'.format(x[i])
                 c3X += ' "{}"'.format(x[i])
-                c3U += ' "{}"'.format(x[i]) 
+                c3U += ' "{}"'.format(x[i])
+                c4G += ' "{}"'.format(x[i])
+                c4Y += ' "{}"'.format(x[i])
+                c4D += ' "{}"'.format(x[i])
+                c4B += ' "{}"'.format(x[i])
+                c4X += ' "{}"'.format(x[i])
+                c4U += ' "{}"'.format(x[i])
+                c5G += ' "{}"'.format(x[i])
+                c5Y += ' "{}"'.format(x[i])
+                c5D += ' "{}"'.format(x[i])
+                c5B += ' "{}"'.format(x[i])
+                c5X += ' "{}"'.format(x[i])
+                c5U += ' "{}"'.format(x[i])
                 if i < len(x) - 1:
                     c1G += " OR"
                     c1Y += " OR"
@@ -268,24 +333,32 @@ def dorks():
                     c1B += " OR"
                     c1X += " OR"
                     c1U += " OR"
-
                     c2G += " OR"
                     c2Y += " OR"
                     c2D += " OR"
                     c2B += " OR"
                     c2X += " OR"
                     c2U += " OR"
-
                     c3G += " OR"
                     c3Y += " OR"
                     c3D += " OR"
                     c3B += " OR"
                     c3X += " OR"
                     c3U += " OR"
+                    c4G += " OR"
+                    c4Y += " OR"
+                    c4D += " OR"
+                    c4B += " OR"
+                    c4X += " OR"
+                    c4U += " OR"
+                    c5G += " OR"
+                    c5Y += " OR"
+                    c5D += " OR"
+                    c5B += " OR"
+                    c5X += " OR"
+                    c5U += " OR"
+
         else:
-            cat1_out += ' "{}"'.format(k)
-            cat2_out += ' "{}"'.format(k)
-            cat3_out += ' "{}"'.format(k)
             c1G += ' "{}"'.format(k)
             c1Y += ' "{}"'.format(k)
             c1D += ' "{}"'.format(k)
@@ -293,20 +366,115 @@ def dorks():
             c1X += ' "{}"'.format(k)
             c1U += ' "{}"'.format(k)
 
-            c2G += ' "{}"'.format(k)
-            c2Y += ' "{}"'.format(k)
-            c2D += ' "{}"'.format(k)
-            c2B += ' "{}"'.format(k)
-            c2X += ' "{}"'.format(k)
-            c2U += ' "{}"'.format(k)
+            c2G += ' intext: {}'.format(k)
+            c2Y += ' {}'.format(k)
+            c2D += ' {}'.format(k)
+            c2B += ' inbody:{}'.format(k)
+            c2X += ' {}'.format(k)
+            c2U += ' {}'.format(k)
 
-            c3G += ' "{}"'.format(k)
+            c3G += ' allintext: {}'.format(k)
             c3Y += ' "{}"'.format(k)
             c3D += ' "{}"'.format(k)
             c3B += ' "{}"'.format(k)
             c3X += ' "{}"'.format(k)
             c3U += ' "{}"'.format(k)
 
+            c4G += ' intitle:{}'.format(k)
+            c4Y += ' intitle:{}'.format(k)
+            c4D += ' intitle:{}'.format(k)
+            c4B += ' intitle:{}'.format(k)
+            c4X += ' "{}"'.format(k)
+            c4U += ' "{}"'.format(k)
+
+            c5G += ' allintitle:{}'.format(k)
+            c5Y += ' "{}"'.format(k)
+            c5D += ' "{}"'.format(k)
+            c5B += ' "{}"'.format(k)
+            c5X += ' "{}"'.format(k)
+            c5U += ' "{}"'.format(k)
+
+
+    # if dork_types == 8:
+    #     output_string += "GOOGLE:       {}\n".format(c1G) # G
+    #     output_string += "YAHOO:        {}\n".format(c1Y) # Y
+    #     output_string += "DUCKDUCKGO:   {}\n".format(c1D) # D
+    #     output_string += "BING:         {}\n".format(c1B) # B
+    #     output_string += "YANDEX:       {}\n".format(c1X) # X
+    #     output_string += "BAIDU:        {}\n".format(c1U) # U
+    #     output_string += "\n"
+    #     output_string += "GOOGLE:       {}\n".format(c2G)
+    #     output_string += "YAHOO:        {}\n".format(c2Y)
+    #     output_string += "DUCKDUCKGO:   {}\n".format(c2D)
+    #     output_string += "BING:         {}\n".format(c2B)
+    #     output_string += "YANDEX:       {}\n".format(c2X)
+    #     output_string += "BAIDU:        {}\n".format(c2U)
+    #     output_string += "\n"
+    #     output_string += "GOOGLE:       {}\n".format(c3G)
+    #     output_string += "YAHOO:        {}\n".format(c3Y)
+    #     output_string += "DUCKDUCKGO:   {}\n".format(c3D)
+    #     output_string += "BING:         {}\n".format(c3B)
+    #     output_string += "YANDEX:       {}\n".format(c3X)
+    #     output_string += "BAIDU:        {}\n".format(c3U)
+    #     output_string += "\n"
+    #     output_string += "GOOGLE:       {}\n".format(c3G)
+    #     output_string += "YAHOO:        {}\n".format(c3Y)
+    #     output_string += "DUCKDUCKGO:   {}\n".format(c3D)
+    #     output_string += "BING:         {}\n".format(c3B)
+    #     output_string += "YANDEX:       {}\n".format(c3X)
+    #     output_string += "BAIDU:        {}\n".format(c3U)
+    #     output_string += "\n"
+    # if misc:
+    #     output_string += "GOOGLE:       {}\n".format(c4G)
+    #     output_string += "YAHOO:        {}\n".format(c4Y)
+    #     output_string += "DUCKDUCKGO:   {}\n".format(c4D)
+    #     output_string += "BING:         {}\n".format(c4B)
+    #     output_string += "YANDEX:       {}\n".format(c4X)
+    #     output_string += "GOOGLE:       {}\n".format(c4U)
+    if dork_types == 7:
+        output_string += "GOOGLE:       {}\n".format(c1G) # G
+        output_string += "YAHOO:        {}\n".format(c1Y) # Y
+        output_string += "DUCKDUCKGO:   {}\n".format(c1D) # D
+        output_string += "BING:         {}\n".format(c1B) # B
+        output_string += "YANDEX:       {}\n".format(c1X) # X
+        output_string += "BAIDU:        {}\n".format(c1U) # U
+        output_string += "\n"
+        output_string += "GOOGLE:       {}\n".format(c2G)
+        # output_string += "YAHOO:        {}\n".format(c2Y)
+        # output_string += "DUCKDUCKGO:   {}\n".format(c2D)
+        output_string += "BING:         {}\n".format(c2B)
+        # output_string += "YANDEX:       {}\n".format(c2X)
+        # output_string += "BAIDU:        {}\n".format(c2U)
+        output_string += "\n"
+        output_string += "GOOGLE:       {}\n".format(c3G)
+        # output_string += "YAHOO:        {}\n".format(c3Y)
+        # output_string += "DUCKDUCKGO:   {}\n".format(c3D)
+        # output_string += "BING:         {}\n".format(c3B)
+        # output_string += "YANDEX:       {}\n".format(c3X)
+        # output_string += "BAIDU:        {}\n".format(c3U)
+        output_string += "\n"
+        output_string += "GOOGLE:       {}\n".format(c4G)
+        output_string += "YAHOO:        {}\n".format(c4Y)
+        output_string += "DUCKDUCKGO:   {}\n".format(c4D)
+        output_string += "BING:         {}\n".format(c4B)
+        # output_string += "YANDEX:       {}\n".format(c4X)
+        # output_string += "BAIDU:        {}\n".format(c4U)
+        output_string += "\n"
+        output_string += "GOOGLE:       {}\n".format(c5G)
+        # output_string += "YAHOO:        {}\n".format(c5Y)
+        # output_string += "DUCKDUCKGO:   {}\n".format(c5D)
+        # output_string += "BING:         {}\n".format(c5B)
+        # output_string += "YANDEX:       {}\n".format(c5X)
+        # output_string += "BAIDU:        {}\n".format(c5U)
+        output_string += "\n"
+
+    if misc:
+        output_string += "GOOGLE:       {}\n".format(c4G)
+        output_string += "YAHOO:        {}\n".format(c4Y)
+        output_string += "DUCKDUCKGO:   {}\n".format(c4D)
+        output_string += "BING:         {}\n".format(c4B)
+        output_string += "YANDEX:       {}\n".format(c4X)
+        output_string += "GOOGLE:       {}\n".format(c4U)
     if dork_types == 4:
         output_string += "GOOGLE:       {}\n".format(c1G) # G
         output_string += "YAHOO:        {}\n".format(c1Y) # Y
@@ -329,33 +497,34 @@ def dorks():
         output_string += "YANDEX:       {}\n".format(c3X)
         output_string += "BAIDU:        {}\n".format(c3U)
         output_string += "\n"
+    if misc:
         output_string += "GOOGLE:       {}\n".format(c4G)
         output_string += "YAHOO:        {}\n".format(c4Y)
         output_string += "DUCKDUCKGO:   {}\n".format(c4D)
         output_string += "BING:         {}\n".format(c4B)
         output_string += "YANDEX:       {}\n".format(c4X)
         output_string += "GOOGLE:       {}\n".format(c4U)
-    if dork_types == 3:
-        output_string += "GOOGLE:       {}\n".format(c1G) # G
-        output_string += "YAHOO:        {}\n".format(c1Y) # Y
-        output_string += "DUCKDUCKGO:   {}\n".format(c1D) # D
-        output_string += "BING:         {}\n".format(c1B) # B
-        output_string += "YANDEX:       {}\n".format(c1X) # X
-        output_string += "BAIDU:        {}\n".format(c1U) # U
-        output_string += "\n"
-        output_string += "GOOGLE:       {}\n".format(c2G)
-        output_string += "YAHOO:        {}\n".format(c2Y)
-        output_string += "DUCKDUCKGO:   {}\n".format(c2D)
-        output_string += "BING:         {}\n".format(c2B)
-        output_string += "YANDEX:       {}\n".format(c2X)
-        output_string += "BAIDU:        {}\n".format(c2U)
-        output_string += "\n"
-        output_string += "GOOGLE:       {}\n".format(c3G)
-        output_string += "YAHOO:        {}\n".format(c3Y)
-        output_string += "DUCKDUCKGO:   {}\n".format(c3D)
-        output_string += "BING:         {}\n".format(c3B)
-        output_string += "YANDEX:       {}\n".format(c3X)
-        output_string += "BAIDU:        {}\n".format(c3U)
+    # if dork_types == 3:
+    #     output_string += "GOOGLE:       {}\n".format(c1G) # G
+    #     output_string += "YAHOO:        {}\n".format(c1Y) # Y
+    #     output_string += "DUCKDUCKGO:   {}\n".format(c1D) # D
+    #     output_string += "BING:         {}\n".format(c1B) # B
+    #     output_string += "YANDEX:       {}\n".format(c1X) # X
+    #     output_string += "BAIDU:        {}\n".format(c1U) # U
+    #     output_string += "\n"
+    #     output_string += "GOOGLE:       {}\n".format(c2G)
+    #     output_string += "YAHOO:        {}\n".format(c2Y)
+    #     output_string += "DUCKDUCKGO:   {}\n".format(c2D)
+    #     output_string += "BING:         {}\n".format(c2B)
+    #     output_string += "YANDEX:       {}\n".format(c2X)
+    #     output_string += "BAIDU:        {}\n".format(c2U)
+    #     output_string += "\n"
+    #     output_string += "GOOGLE:       {}\n".format(c3G)
+    #     output_string += "YAHOO:        {}\n".format(c3Y)
+    #     output_string += "DUCKDUCKGO:   {}\n".format(c3D)
+    #     output_string += "BING:         {}\n".format(c3B)
+    #     output_string += "YANDEX:       {}\n".format(c3X)
+    #     output_string += "BAIDU:        {}\n".format(c3U)
     elif dork_types == 2:
         output_string += "GOOGLE:       {}\n".format(c1G)
         output_string += "YAHOO:        {}\n".format(c1Y)
@@ -370,13 +539,13 @@ def dorks():
         output_string += "BING:         {}\n".format(c2B)
         output_string += "YANDEX:       {}\n".format(c2X)
         output_string += "BAIDU:        {}\n".format(c2U)
-    elif dork_types == 1:
-        output_string += "GOOGLE:       {}\n".format(c1G)
-        output_string += "YAHOO:        {}\n".format(c1Y)
-        output_string += "DUCKDUCKGO:   {}\n".format(c1D)
-        output_string += "BING:         {}\n".format(c1B)
-        output_string += "YANDEX:       {}\n".format(c1X)
-        output_string += "BAIDU:        {}\n".format(c1U)
+    # elif dork_types == 1:
+    #     output_string += "GOOGLE:       {}\n".format(c1G)
+    #     output_string += "YAHOO:        {}\n".format(c1Y)
+    #     output_string += "DUCKDUCKGO:   {}\n".format(c1D)
+    #     output_string += "BING:         {}\n".format(c1B)
+    #     output_string += "YANDEX:       {}\n".format(c1X)
+    #     output_string += "BAIDU:        {}\n".format(c1U)
     return output_string
 
 def output():
@@ -407,32 +576,34 @@ def output():
 
 # determine what params are being used
 # THIS IS ONE FUNCTION
-if s is not None or i is not None or a is not None or d is not None:
+if s is not None: #or i is not None or a is not None or d is not None:
     arg_count     += 1
     dork_types     = 4
-    if s is not None:
-        i = a = d = s
-    elif i is not None:
-        s = a = d = i
-    elif a is not None:
-        s = i = d = a
-    elif d is not None:
-        s = i = a = d 
+    misc           = True
+    # if s is not None:
+    #     i = a = d = s
+    # elif i is not None:
+    #     s = a = d = i
+    # elif a is not None:
+    #     s = i = d = a
+    # elif d is not None:
+    #     s = i = a = d 
+
 
 # THIS IS ANOTHER FUNCTION
-if f is not None or e is not None:
+if f is not None: # or e is not None:
     arg_count += 1
     if dork_types < 2:
         dork_types = 2
-    if f is not None:
-        e = f
-    elif e is not None:
-        f = e
+    # if f is not None:
+    #     e = f
+    # elif e is not None:
+    #     f = e
 
 if k is not None:
     arg_count += 1
-    if dork_types == 0:
-        dork_types = 1
+    if dork_types < 7:
+        dork_types = 7        
 
 output()
 
